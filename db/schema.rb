1a61c9cd4c939be1c552b820d4146a294e33ba3d
hata-junnosuke <jh19911209@icloud.com>
2021-05-31 11:30:55 +0000

    goalカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_102312) do
+ActiveRecord::Schema.define(version: 2021_05_31_104816) do
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_05_15_102312) do
     t.string "profie_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "goal"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
