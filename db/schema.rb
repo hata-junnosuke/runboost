33a21c236d39d6f26dd044ae304141e0cba4dc9b
hata-junnosuke <jh19911209@icloud.com>
2021-07-31 12:21:59 +0000

    コメント

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_112238) do
+ActiveRecord::Schema.define(version: 2021_07_31_115426) do
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
