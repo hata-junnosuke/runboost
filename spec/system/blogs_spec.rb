973520d86f71970a8e6913e50261fdf353365ad5
hata-junnosuke <jh19911209@icloud.com>
2021-09-20 18:44:06 +0900

    docker編集

--- spec/system/blogs_spec.rb
+++ spec/system/blogs_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "Blogs", type: :system do
 
   scenario "user creates a new blog" do
     visit new_user_registration_path
-    fill_in "Eメール", with: user.email
+    fill_in "Eメール", with: @user.email
     fill_in "パスワード", with: user.password
     click_button "ログイン"
     
