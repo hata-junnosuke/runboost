5275cfd09fe35a31a759f1e13003ea20c23b8503
hata-junnosuke <jh19911209@icloud.com>
2021-09-24 17:06:14 +0900

    Blogモデルにテストを追加

--- spec/factories/blogs.rb
+++ spec/factories/blogs.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :blog do
-    
+    id { 1 }
+    user_id { 1 }
+    content {"ジョグ"}
+    distance { 10 }
+    start_time {"2021-09-24"}
+    comment {"お疲れ様でした"}
   end
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # 有効なファクトリを持つこと
+  it "has a valid factory" do
+    expect(FactoryBot.build(:blog)).to be_valid
+  end
+
+  # contentの入力がなければ無効な状態であること
+  it "is invalid without content" do
+    blog = FactoryBot.build(:blog, content: nil)
+    blog.valid?
+    expect(blog.errors[:content]).to include("を入力してください")
+  end
 end
