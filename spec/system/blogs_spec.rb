28a081bb7e81d0e4f650fc2fc8fb7dda9707c252
hata-junnosuke <jh19911209@icloud.com>
2021-10-10 20:48:36 +0900

    systemspec追加

--- spec/system/blogs_spec.rb
+++ spec/system/blogs_spec.rb
@@ -7,21 +7,19 @@ RSpec.describe "Blogs", type: :system do
   end
 
   scenario "user creates a new blog" do
-    visit new_user_registration_path
-    fill_in "", with: @user.email
-    fill_in "パスワード", with: user.password
+    visit new_user_session_path
+
+    fill_in "sesstion_email", with: @user.email
+    fill_in "sesstion_password", with: @user.password
     click_button "ログイン"
     
     expect {
-     click_link "記録する"
-     fill_in "日時", with: "2021/09/10"
-     fill_in "メニュー", with: "朝ラン"
-     fill_in "距離", with: "10"
+     click_link "記録する", id: "write"
+     fill_in "content", with: "朝ラン"
+     fill_in "distance", with: "10"
      click_button "記録する"
 
     expect(page).to have_content "記録しました。"
-    expect(page).to have_content "朝ラン"
-    expect(page).to have_content "Owner: #{user.name}"
-    }.to change(user.blogs, :count).by(1)
+    }.to change(@user.blogs, :count).by(1)
   end  
 end
