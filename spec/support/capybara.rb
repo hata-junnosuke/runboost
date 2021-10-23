5b5f47bbef38461c7ed6e7c86f3f15450d813ad1
hata-junnosuke <jh19911209@icloud.com>
2021-10-23 12:01:03 +0900

    racktest変更

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,2 +1,9 @@
 # frozen_string_literal: true
-Capybara.javascript_driver = :selenium_chrome_headless
+#Capybara.javascript_driver = :selenium_chrome_headless
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+end
+Capybara.default_driver = :rack_test
