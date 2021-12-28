15f405ff1b0c9b7cf27aa3f1a8d5beb1a3a01a6e
hata-junnosuke <jh19911209@icloud.com>
2021-12-28 15:53:32 +0900

    bundle

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_28_044800) do
+ActiveRecord::Schema.define(version: 2021_12_28_061534) do
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "user_id", null: false
