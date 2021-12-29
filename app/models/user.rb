24dbf817f4a058ae039f103455891f9280d4ce0c
hata-junnosuke <jh19911209@icloud.com>
2021-12-29 11:49:15 +0900

    README更新、メールアドレス制約

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, presence: true, length: {maximum: 15 }
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+            uniqueness: { case_sensitive: false }
   validates :profile, length: { maximum: 255 }
   has_many :blogs, dependent: :destroy
   has_many :goals, dependent: :destroy
