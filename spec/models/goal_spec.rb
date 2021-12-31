f285c82cfa22b48d0d2658c685395107623006d6
hata-junnosuke <jh19911209@icloud.com>
2021-12-31 13:43:43 +0900

    buildからcreate

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Goal, type: :model do
   it '有効なファクトリを持つこと' do
-    expect(FactoryBot.build(:goal)).to be_valid
+    expect(FactoryBot.create(:goal)).to be_valid
   end
 end
